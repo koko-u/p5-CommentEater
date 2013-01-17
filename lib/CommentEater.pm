4516b09be06ad9256ebeab47f53d84c3712a5a0c
Kozaki, Tsuneaki <kozaki.tsuneaki@gmail.com>
2013-01-17 19:50:07 +0900

    initial commit

---
+++ lib/CommentEater.pm
@@ -0,0 +1,32 @@
+package CommentEater;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+CommentEater -
+
+=head1 SYNOPSIS
+
+  use CommentEater;
+
+=head1 DESCRIPTION
+
+CommentEater is
+
+=head1 AUTHOR
+
+KOZAKI Tsuneaki E<lt>kozaki.tsuneaki@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
